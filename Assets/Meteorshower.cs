13b0877de26031d863e62a1e0a8b474e0f2bea56
caident251 <caident251@gmail.com>
2024-10-05 17:44:28 -0400

    M

--- Assets/Meteorshower.cs
+++ Assets/Meteorshower.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Meteorshower : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject Meteor;
+       // Start is called before the first frame update
     void Start()
     {
         
@@ -13,10 +14,28 @@ public class Meteorshower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        int key = GetComponent<Combat>().CurSkill;
-        if (Input.GetKeyDown(""+key+""))
+        RaycastHit hit;
+        if(Physics.Raycast(Camera.main.gameObject.transform.position,Camera.main.gameObject.transform.forward, out hit,50f))
         {
+        if (Input.GetMouseButtonDown(1) ||Input.GetMouseButtonDown(0))
+        { 
+                 StartCoroutine( Shower(hit.point));
+                }
+            }
+        
+      
 
-        }
     }
+    IEnumerator Shower(Vector3 v)
+{
+    for(int i = 0;i<18;i++)
+    {
+        if(i==17){
+            this.enabled =false;
+        }
+                        GameObject b = Instantiate(Meteor,v + new Vector3(0,50,0)+Random.onUnitSphere*10,Quaternion.identity);
+                b.GetComponent<DamageDealer>().Source = gameObject;
+        yield return new WaitForSeconds(Random.Range(0.5f,1));
+    }
+}
 }
